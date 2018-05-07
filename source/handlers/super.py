11d41df4e540bf19c72ca43223bddb103f90bba7
晴识明月 <1593674921@qq.com>
2018-05-07 15:56:55 +0800

    super页面路由

--- source/handlers/super.py
+++ source/handlers/super.py
@@ -5,6 +5,15 @@ from handlers.base.pub_web import AdminBaseHandler
 from handlers.base.pub_func import QuryListDictFunc, TimeFunc, check_float, PubMethod
 import requests, json , datetime
 
+@AdminBaseHandler.check_arguments("action?:str")
 class Home(AdminBaseHandler):
     def get(self):
-        return self.render("super/DataManager.html")
+    	action=self.args.get("action","")
+    	if action=="manage_data":
+        	return self.render("super/DataManager.html")
+        elif action=="manage_admin":
+        	return self.render("super/AdminManager.html")
+        else action=="manage_operator":
+        	return self.render("super/OperatorManager.html")
+        else:
+        	return self.send_fail(403)
